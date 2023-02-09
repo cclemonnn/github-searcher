9552305094d57fe6ae4be295289998360b756155
cclemonnn <petersocool0313@gmail.com>
2023-02-10 01:42:06 +0800

    renamed css module

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,23 +1,23 @@
 import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import NavbarCSS from "./Navbar.module.css";
+import n from "./Navbar.module.css";
 
 function Navbar() {
   return (
-    <nav className={NavbarCSS.navbar}>
-      <div className={NavbarCSS.navContainer}>
-        <div className={NavbarCSS.iconContainer}>
-          <FaGithub className={NavbarCSS.githubIcon} />
-          <Link to="/" className={NavbarCSS.searcher}>
+    <nav className={n.navbar}>
+      <div className={n.navContainer}>
+        <div className={n.iconContainer}>
+          <FaGithub className={n.githubIcon} />
+          <Link to="/" className={n.searcher}>
             Github Searcher
           </Link>
         </div>
 
-        <div className={NavbarCSS.btnContainer}>
-          <Link to="/" className={NavbarCSS.btn}>
+        <div className={n.btnContainer}>
+          <Link to="/" className={n.btn}>
             Home
           </Link>
-          <Link to="/about" className={NavbarCSS.btn}>
+          <Link to="/about" className={n.btn}>
             About
           </Link>
         </div>

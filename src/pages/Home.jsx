a144ee249ccc1e18708c6b57124d3acadcbffaca
cclemonnn <petersocool0313@gmail.com>
2023-02-13 04:00:26 +0800

    moved grid container to UserResults

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import UserResults from "../components/users/UserResults";
-import h from "./Home.module.css";
 
 function Home() {
   return (
-    <div className={h.container}>
+    <>
       <UserResults />
-    </div>
+    </>
   );
 }
 export default Home;

b2a18b364fc19aabb0f39f5906f2c33f30c33daa
cclemonnn <petersocool0313@gmail.com>
2023-02-20 03:11:03 +0800

    added AlertProvider

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 function App() {
   return (
     <GithubProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Main>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/notfound" element={<NotFound />}></Route>
-            <Route path="/*" element={<NotFound />}></Route>
-          </Routes>
-        </Main>
-        <Footer />
-      </BrowserRouter>
+      <AlertProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Main>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+              <Route path="/notfound" element={<NotFound />}></Route>
+              <Route path="/*" element={<NotFound />}></Route>
+            </Routes>
+          </Main>
+          <Footer />
+        </BrowserRouter>
+      </AlertProvider>
     </GithubProvider>
   );
 }

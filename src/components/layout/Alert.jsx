0d90dfef808086e73eed896aadd262bea70872fc
cclemonnn <petersocool0313@gmail.com>
2023-02-21 02:45:16 +0800

    added alert syles

--- src/components/layout/Alert.jsx
+++ src/components/layout/Alert.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import AlertContext from "../../context/alert/AlertContext";
+import a from "./Alert.module.css";
 
 function Alert() {
   const { alert } = useContext(AlertContext);
 
-  if (alert !== null) {
-    return <div>{alert.msg}</div>;
-  } else {
-    return <div className=""></div>;
-  }
+  return (
+    <div className={`${a.alert} ${alert !== null && a.show} ${a.error}`}>
+      {alert !== null && alert.msg}
+    </div>
+  );
 }
 export default Alert;

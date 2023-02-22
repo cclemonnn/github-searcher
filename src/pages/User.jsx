32c59764410c1ae1c161d7b28539851a48d2e801
cclemonnn <petersocool0313@gmail.com>
2023-02-23 02:50:06 +0800

    added avatar overlay and github link

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useContext } from "react";
 import GithubContext from "../context/github/GithubContext";
 import { useParams } from "react-router-dom";
-import { FaSpinner } from "react-icons/fa";
+import { FaSpinner, FaGithub, FaTwitter } from "react-icons/fa";
+import { CgWebsite } from "react-icons/cg";
 import { MdEmail } from "react-icons/md";
 import u from "./User.module.css";
 
@@ -12,7 +13,16 @@ function User() {
   const params = useParams();
 
   // User items
-  const { name, login, avatar_url, email, bio } = user;
+  const {
+    name,
+    login,
+    avatar_url,
+    email,
+    bio,
+    html_url,
+    twitter_username,
+    blog,
+  } = user;
 
   // Get user data once
   useEffect(() => {
@@ -29,14 +39,45 @@ function User() {
 
   return (
     <div className={u.profileContainer}>
-      <figure className={u.avatar}>
-        <img src={avatar_url} alt="user avatar" />
+      {/* Avatar */}
+      <figure className={u.avatarContainer}>
+        <img src={avatar_url} alt="user avatar" className={u.avatar} />
         <figcaption>hover for more information</figcaption>
+        <div className={u.overlayContainer}>
+          <div className={u.twitterContainer}>
+            <FaTwitter className={u.twitterIcon} />
+            {twitter_username !== null ? (
+              <a
+                href={`https://twitter.com/${twitter_username}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className={u.twitter}>{twitter_username}</div>
+              </a>
+            ) : (
+              <div className={u.noTwitter}>
+                (<i>Twitter not provided</i>)
+              </div>
+            )}
+          </div>
+          <div className={u.blogContainer}>
+            <CgWebsite className={u.blogIcon} />
+            {blog !== "" ? (
+              <div className={u.blog}>{blog}</div>
+            ) : (
+              <div className={u.noBlog}>
+                (<i>Blog not provided</i>)
+              </div>
+            )}
+          </div>
+        </div>
       </figure>
+
+      {/* Profile Info */}
       <div className={u.profileInfo}>
         {/* Name */}
         <div className={u.nameContainer}>
-          {name !== null && <span className={u.name}>{name}&nbsp;&nbsp;</span>}
+          {name !== null && <span className={u.name}>{name}&nbsp;</span>}
           <span className={u.login}>
             (<i>{login}</i>)
           </span>
@@ -54,6 +95,14 @@ function User() {
           )}
         </div>
 
+        {/* Github link */}
+        <div className={u.gitContainer}>
+          <FaGithub className={u.gitIcon} />
+          <a href={html_url} target="_blank" rel="noreferrer">
+            <div className={u.gitText}>Visit Github Page</div>
+          </a>
+        </div>
+
         {/* User bio */}
         <div className={u.bioContainer}>
           <div className={u.aboutMe}>About Me:</div>
@@ -61,7 +110,7 @@ function User() {
             <div className={u.bio}>{bio}</div>
           ) : (
             <div className={u.noBio}>
-              (<i>No bio provided</i>)
+              (<i>Bio not provided</i>)
             </div>
           )}
         </div>

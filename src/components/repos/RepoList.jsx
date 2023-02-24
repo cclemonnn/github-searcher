a6822f6d60611882f0c4be60a1c77a90fcaf9459
cclemonnn <petersocool0313@gmail.com>
2023-02-25 03:39:14 +0800

    added RepoList content and styles

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,9 +1,28 @@
+import { HiExternalLink } from "react-icons/hi";
+import r from "./RepoList.module.css";
+
 function RepoList({ repos }) {
   return (
-    <div>
-      {repos.map((repo) => (
-        <div>{repo.name}</div>
-      ))}
+    <div className={r.repoContainer}>
+      {repos.length > 0 ? (
+        <>
+          <div
+            className={r.repoTitle}
+          >{`Showing Latest ${repos.length} Updated Repos`}</div>
+          {repos.map((repo) => (
+            <a href={repo.html_url} target="_blank" rel="noreferer">
+              <div className={r.innerContaienr}>
+                <HiExternalLink className={r.linkIcon} />
+                <div className={r.repoName}>{repo.name}</div>
+              </div>
+            </a>
+          ))}
+        </>
+      ) : (
+        <div className={r.noRepo}>
+          (<i>No public repo</i>)
+        </div>
+      )}
     </div>
   );
 }

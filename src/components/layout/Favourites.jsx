c661a88dd69c9ceb01a75c68cf6f8f3307ebd788
cclemonnn <petersocool0313@gmail.com>
2023-02-27 04:21:03 +0800

    changed Footer to Favourites

---
+++ src/components/layout/Favourites.jsx
@@ -0,0 +1,6 @@
+import f from "./Favourites.module.css";
+
+function Footer() {
+  return <div className={f.footer}>Favourites: </div>;
+}
+export default Footer;
--- src/components/layout/Footer.jsx
+++
@@ -1,6 +0,0 @@
-import f from "./Footer.module.css";
-
-function Footer() {
-  return <div className={f.footer}>Footer</div>;
-}
-export default Footer;

c249225f27644815961a5e9e4d7ae0170ba893da
cclemonnn <petersocool0313@gmail.com>
2023-02-27 05:49:32 +0800

    init star functions before moving them to context

--- src/components/star/Star.jsx
+++ src/components/star/Star.jsx
@@ -1,11 +1,40 @@
+import { useState } from "react";
 import { FaStar, FaRegStar } from "react-icons/fa";
 import s from "./Star.module.css";
 
-function Star({ style }) {
+function Star({ style, login }) {
+  const [favList, setFavList] = useState(new Set());
+
+  // Check if user in list
+  const userInList = (user) => favList.has(user);
+
+  // Add user to list
+  const addUser = (user) => {
+    setFavList((prev) => new Set([...prev, user]));
+  };
+
+  // Remove user from list
+  const removeUser = (user) => {
+    const newFavList = new Set(favList);
+    newFavList.delete(user);
+    setFavList(newFavList);
+  };
+
   return (
     <>
-      {/* <FaStar className={s.star} /> */}
-      <FaRegStar className={s.star} style={style} />
+      {userInList(login) ? (
+        <FaStar
+          className={s.star}
+          style={style}
+          onClick={() => removeUser(login)}
+        />
+      ) : (
+        <FaRegStar
+          className={s.star}
+          style={style}
+          onClick={() => addUser(login)}
+        />
+      )}
     </>
   );
 }

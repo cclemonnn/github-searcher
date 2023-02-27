01781ce982563daefbedcaed50d865b1ca007f07
cclemonnn <petersocool0313@gmail.com>
2023-02-28 02:25:59 +0800

    added user box and styles

--- src/components/layout/Favourites.jsx
+++ src/components/layout/Favourites.jsx
@@ -1,6 +1,26 @@
+import { useContext } from "react";
+import FavListContext from "../../context/favList/FavListContext";
+import { RiDeleteBack2Fill } from "react-icons/ri";
 import f from "./Favourites.module.css";
 
-function Footer() {
-  return <div className={f.footer}>Favourites: </div>;
+function Favourites() {
+  const { favList, removeUser } = useContext(FavListContext);
+
+  return (
+    <div className={f.favContainer}>
+      <div className={f.favText}>Favourites ({favList.size}):</div>
+      {[...favList].map((user) => {
+        return (
+          <div key={user} className={f.userContainer}>
+            <div className={f.user}>{user}</div>
+            <RiDeleteBack2Fill
+              className={f.removeIcon}
+              onClick={() => removeUser(user)}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
-export default Footer;
+export default Favourites;

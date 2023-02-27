70b65ad147af0351844f3e68ea01873b07953c0c
cclemonnn <petersocool0313@gmail.com>
2023-02-28 03:18:09 +0800

    added green and red alerts

--- src/components/layout/Alert.jsx
+++ src/components/layout/Alert.jsx
@@ -6,7 +6,11 @@ function Alert() {
   const { alert } = useContext(AlertContext);
 
   return (
-    <div className={`${a.alert} ${alert !== null && a.show} ${a.error}`}>
+    <div
+      className={`${a.alert} ${alert !== null && a.show} ${
+        alert !== null && alert.type === "red" && a.red
+      } ${alert !== null && alert.type === "green" && a.green}`}
+    >
       {alert !== null && alert.msg}
     </div>
   );

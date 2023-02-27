08a63c974f5049a94070c0e1b1aa83b46ef17cf7
cclemonnn <petersocool0313@gmail.com>
2023-02-28 03:19:24 +0800

    added setAlert

--- src/context/favList/FavListContext.js
+++ src/context/favList/FavListContext.js
@@ -1,9 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
+import AlertContext from "../alert/AlertContext";
 
 const FavListContext = createContext();
 
 export const FavListProvider = ({ children }) => {
   const [favList, setFavList] = useState(new Set());
+  const { setAlert } = useContext(AlertContext);
 
   // Check if user in list
   const userInList = (user) => favList.has(user);
@@ -11,6 +13,9 @@ export const FavListProvider = ({ children }) => {
   // Add user to list
   const addUser = (user) => {
     setFavList((prev) => new Set([...prev, user]));
+
+    // show alert
+    setAlert(`${user} added to Favourites`, "green");
   };
 
   // Remove user from list
@@ -18,6 +23,9 @@ export const FavListProvider = ({ children }) => {
     const newFavList = new Set(favList);
     newFavList.delete(user);
     setFavList(newFavList);
+
+    // show alert
+    setAlert(`${user} removed from Favourites`, "green");
   };
 
   return (

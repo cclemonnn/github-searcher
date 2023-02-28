aecbcbab76dabe82c6cc0cc0288ff8ddecc73ea3
cclemonnn <petersocool0313@gmail.com>
2023-03-01 04:48:34 +0800

    added PageProvider

--- src/App.js
+++ src/App.js
@@ -11,29 +11,32 @@ import Alert from "./components/layout/Alert";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
 import { FavListProvider } from "./context/favList/FavListContext";
+import { PageProvider } from "./context/page/PageContext";
 
 function App() {
   return (
-    <GithubProvider>
-      <AlertProvider>
-        <FavListProvider>
-          <BrowserRouter>
-            <Navbar />
-            <Alert />
-            <Main>
-              <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/about" element={<About />}></Route>
-                <Route path="/user/:login" element={<User />}></Route>
-                <Route path="/notfound" element={<NotFound />}></Route>
-                <Route path="/*" element={<NotFound />}></Route>
-              </Routes>
-            </Main>
-            <Favourites />
-          </BrowserRouter>
-        </FavListProvider>
-      </AlertProvider>
-    </GithubProvider>
+    <PageProvider>
+      <GithubProvider>
+        <AlertProvider>
+          <FavListProvider>
+            <BrowserRouter>
+              <Navbar />
+              <Alert />
+              <Main>
+                <Routes>
+                  <Route path="/" element={<Home />}></Route>
+                  <Route path="/about" element={<About />}></Route>
+                  <Route path="/user/:login" element={<User />}></Route>
+                  <Route path="/notfound" element={<NotFound />}></Route>
+                  <Route path="/*" element={<NotFound />}></Route>
+                </Routes>
+              </Main>
+              <Favourites />
+            </BrowserRouter>
+          </FavListProvider>
+        </AlertProvider>
+      </GithubProvider>
+    </PageProvider>
   );
 }
 
